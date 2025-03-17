53278858cc1e4e7813b4be8d2dae032c3b817ccb
monjara <yaginumtt@gmail.com>
2025-03-17 14:07:37 +0900

    editor into workspace

--- crates/sticky/src/main.rs
+++ crates/sticky/src/main.rs
@@ -1,14 +1,12 @@
-mod view;
-
+use editor::Editor;
 use gpui::{App, Application, WindowOptions};
-use view::editor::Editor;
 
 fn main() {
     Application::new().run(|cx: &mut App| {
         gpui_component::init(cx);
 
         cx.open_window(WindowOptions::default(), |window, cx| {
-           Editor::view(window, cx)
+            Editor::view(window, cx)
         })
         .unwrap();
     });
