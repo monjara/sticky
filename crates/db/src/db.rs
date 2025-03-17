21b87d3e0f8add4f8df6af095bd80bc79c7ab9e8
monjara <yaginumtt@gmail.com>
2025-03-17 15:24:02 +0900

    sample connection

--- crates/db/src/db.rs
+++ crates/db/src/db.rs
@@ -1,14 +1,113 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use rusqlite::{Connection, Result};
+use ulid::Ulid;
+
+#[derive(Debug)]
+struct Note {
+    id: String,
+    title: String,
+    body: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn init() -> Result<()> {
+    let path = "./database.sqlite";
+    let conn = Connection::open(path)?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    conn.execute(
+        "
+            CREATE TABLE IF NOT EXISTS notes (
+              id TEXT NOT NULL
+            , title TEXT NOT NULL
+            , body TEXT NOT NULL
+            );
+        ",
+        (),
+    )?;
+
+    let note = Note {
+        id: Ulid::new().to_string(),
+        title: "first note".to_string(),
+        body: r"
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+
+
+            asdfasdfa
+            asdfasdfa
+            "
+        .to_string(),
+    };
+
+    conn.execute(
+        "
+        insert into notes (id, title, body) values (?1, ?2, ?3);
+        ",
+        (&note.id, &note.title, &note.body),
+    )?;
+
+    let note = Note {
+        id: Ulid::new().to_string(),
+        title: "second note".to_string(),
+        body: r"
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+
+
+            asdfasdfa
+            asdfasdfa
+            "
+        .to_string(),
+    };
+
+    conn.execute(
+        "
+        insert into notes (id, title, body) values (?1, ?2, ?3);
+        ",
+        (&note.id, &note.title, &note.body),
+    )?;
+
+    let note = Note {
+        id: Ulid::new().to_string(),
+        title: "third note".to_string(),
+        body: r"
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+            asdfasdfa
+
+
+            asdfasdfa
+            asdfasdfa
+            "
+        .to_string(),
+    };
+
+    conn.execute(
+        "
+        insert into notes (id, title, body) values (?1, ?2, ?3);
+        ",
+        (&note.id, &note.title, &note.body),
+    )?;
+
+    let mut stmt = conn.prepare("select id, title, body from notes order by id desc")?;
+
+    let notes = stmt.query_map([], |row| {
+        Ok(Note {
+            id: row.get(0)?,
+            title: row.get(1)?,
+            body: row.get(2)?,
+        })
+    })?;
+
+    for note in notes {
+        println!("{note:?}");
     }
+
+    Ok(())
 }
--- crates/sticky/src/main.rs
+++ crates/sticky/src/main.rs
@@ -4,6 +4,7 @@ use gpui::{App, Application, WindowOptions};
 fn main() {
     Application::new().run(|cx: &mut App| {
         gpui_component::init(cx);
+        db::init().unwrap();
 
         cx.open_window(WindowOptions::default(), |window, cx| {
             Editor::view(window, cx)
