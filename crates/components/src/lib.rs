1cd25ecc3e861bc7d50156aa7fa88e6a3d15fc5c
monjara <yaginumtt@gmail.com>
2025-03-25 19:23:11 +0900

    first commit for list window

--- crates/components/src/lib.rs
+++ crates/components/src/lib.rs
@@ -3,6 +3,7 @@ use input::text_input;
 
 pub mod input;
 pub mod scroll;
+pub mod list;
 
 pub fn init(cx: &mut App) {
     text_input::init(cx);
--- crates/window/src/list.rs
+++ crates/window/src/list.rs
@@ -1,1 +1,3 @@
+pub struct ListWindow {}
 
+impl ListWindow {}
